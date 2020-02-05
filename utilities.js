f176a08a2b03c928bd0fcc7b0ff71a5eef7c6ff4
Sai Manikanta Godavarthi <saig2304@gmail.com>
2020-02-05 11:14:43 -0600

    Update utilities.js

--- utilities.js
+++ utilities.js
@@ -4,3 +4,13 @@
         : null)).filter(item => item);
       return chunckedArrays;
     };
+
+// eslint-disable-next-line consistent-return
+export const getMapKeyByValue = (map, searchValue) => {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const [key, value] of map.entries()) {
+    if (value === searchValue) { return key; }
+  }
+};
+
+export const buildKey = (...params) => params.join('-');
